bf96dfa4180c947c8c81640eab5f5958a07202b4
Cédric Messiant <cedricmessiant@ecreall.com>
2014-09-18 14:28:19 +0200

    Preparing release 0.6

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-0.6 (unreleased)
+0.6 (2014-09-18)
 ----------------
 
 - Coverage and integration with coveralls.
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '0.6.dev0'
+version = '0.6'
 
 setup(
     name='bobtemplates.ecreall',

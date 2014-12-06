3603d236ada726841deb25f769fad7dbfa209e9a
Philip Bauer <bauer@starzel.de>
2014-12-07 00:48:32 +0100

    Back to development: 0.2

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,6 +1,12 @@
 Changelog
 =========
 
+0.2 (unreleased)
+----------------
+
+- Nothing changed yet.
+
+
 0.1 (2014-12-07)
 ----------------
 
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '0.1'
+version = '0.2.dev0'
 
 setup(
     name='bobtemplates.plone',

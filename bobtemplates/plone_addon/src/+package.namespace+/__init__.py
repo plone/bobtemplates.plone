a29686f05fd1041663237603df42b3c57ee77c98
Daniel Jowett <daniel@jowettenterprises.com>
2015-07-01 11:40:16 +0100

    Assume setuptools is always available

--- bobtemplates/plone_addon/src/+package.namespace+/__init__.py
+++ bobtemplates/plone_addon/src/+package.namespace+/__init__.py
@@ -1,7 +1,2 @@
 # -*- coding: utf-8 -*-
-# See http://peak.telecommunity.com/DevCenter/setuptools#namespace-packages
-try:
-    __import__('pkg_resources').declare_namespace(__name__)
-except ImportError:
-    from pkgutil import extend_path
-    __path__ = extend_path(__path__, __name__)
+__import__('pkg_resources').declare_namespace(__name__)

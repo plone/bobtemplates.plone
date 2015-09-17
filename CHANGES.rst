d78b0646175fb0ecd57ce56c2a46108d9e956a86
Timo Stollenwerk <tisto@plone.org>
2015-09-17 18:10:01 +0200

    Back to development: 1.0

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,6 +1,12 @@
 Changelog
 =========
 
+1.0 (unreleased)
+----------------
+
+- Nothing changed yet.
+
+
 1.0b1 (2015-09-17)
 ------------------
 
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '1.0b1'
+version = '1.0.dev0'
 
 long_description = (
     open('README.rst').read() + '\n' +

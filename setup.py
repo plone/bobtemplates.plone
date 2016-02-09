1e0be4dd762e66484797b7835923868641eea47e
Johannes Raggam <thetetet@gmail.com>
2016-02-09 12:42:38 +0100

    remove parenthesis

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ from setuptools import setup
 version = '1.0.2.dev0'
 
 
-long_description = ('\n'.join([
+long_description = '\n'.join([
     open('README.rst').read(),
     open('CONTRIBUTORS.rst').read(),
     open('CHANGES.rst').read(),
-]))
+])
 
 
 setup(

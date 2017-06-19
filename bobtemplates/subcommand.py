167bc9913f5f7b6c2411cd26888ab7c73472d9fb
MrTango <md@derico.de>
2017-06-19 12:19:31 +0300

    automatically add types to type.xml

--- bobtemplates/subcommand.py
+++ bobtemplates/subcommand.py
@@ -1,4 +1,5 @@
 from mrbob.bobexceptions import ValidationError
+from lxml import etree
 import keyword
 import logging
 import re
@@ -43,9 +44,39 @@ def prepare_render(configurator):
         'dexterity_type_name_normalized'] = type_name.replace(' ', '_').lower()
 
 
+def _update_types_xml(configurator):
+    """ Add the new type to types.xml in Generic Setup profiles.
+    """
+    types_file_name = u'types.xml'
+    types_file_dir = u'profiles/default'
+    types_file_path = types_file_dir + '/' + types_file_name
+    file_list = os.listdir(
+        os.path.dirname(types_file_path))
+    if types_file_name not in file_list:
+        os.rename(types_file_dir + '/types.xml.example', types_file_path)
+
+    with open(types_file_path, 'r') as xml_file:
+        parser = etree.XMLParser(remove_blank_text=True)
+        tree = etree.parse(xml_file, parser)
+        types = tree.xpath("/object[@name='portal_types']")[0]
+        type_name = configurator.variables['dexterity_type_name_normalized']
+        if len(types.xpath("./object[@name='%s']" % type_name)):
+            print("%s already in types.xml, skip adding!" % type_name)
+            return
+        types.append(
+            etree.Element('object', name=type_name, meta_type='Dexterity FTI'))
+
+    with open(types_file_path, 'w') as xml_file:
+        tree.write(
+            xml_file, pretty_print=True, xml_declaration=True,
+            encoding="utf-8")
+
+
 def post_renderer(configurator):
     """
     """
+    _update_types_xml(configurator)
+
     print("""vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 
 Now add the follwing to profiles/default/types.xml:

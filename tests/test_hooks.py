13a257dd59d7dcf1d96d52a09200f50f37213bf5
Alexander Loechel <Alexander.Loechel@lmu.de>
2017-07-06 19:40:58 +0300

    more tests

--- tests/test_hooks.py
+++ tests/test_hooks.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from bobtemplates import hooks
+from mrbob.bobexceptions import SkipQuestion
 from mrbob.bobexceptions import ValidationError
 from mrbob.configurator import Configurator
 
@@ -7,64 +8,7 @@ import glob
 import os
 import os.path
 import pytest
-import shutil
 import subprocess
-import tempfile
-import unittest
-
-
-def generate_plone_addon_template(path,
-                                  root_namespace,
-                                  nested_namespace,
-                                  name
-                                  ):
-    template = """[variables]
-package.type = Basic
-package.dexterity_type_name =
-package.namespace = {root_namespace}
-package.namespace2 = {nested_namespace}
-package.name = {name}
-package.description = Dummy package
-package.example = True
-
-author.name = The Plone Collective
-author.email = collective@plone.org
-author.github.user = collective
-author.irc = irc.freenode.org#plone
-
-plone.version = 5-latest
-""".format(root_namespace=root_namespace,
-           nested_namespace=nested_namespace,
-           name=name)
-    with open(os.path.join(path, 'answers.ini'), 'w') as f:
-        f.write(template)
-
-
-base_files = [
-    '.mrbob.ini',
-    '.editorconfig',
-    'setup.py',
-    'setup.cfg',
-]
-
-
-addon_files = [
-    'src/__init__.py'
-]
-
-
-def test_plone_addon_generation(tmpdir):
-    generate_plone_addon_template(tmpdir.strpath, 'collective', '', 'foo')
-    result = subprocess.call(
-        ['mrbob', '-O', 'collective.foo', 'bobtemplates:plone_addon',
-         '--config', 'answers.ini'], cwd=tmpdir.strpath
-    )
-    assert result == 0
-    generated_files = glob.glob(tmpdir.strpath + '/collective.foo/*')
-    length = len(tmpdir.strpath + '/collective.foo/')
-    generated_files = [f[length:] for f in generated_files]
-    required_files = base_files + addon_files
-    assert required_files <= generated_files
 
 
 def test_to_boolean():
@@ -87,6 +31,130 @@ def test_to_boolean():
     # Error Check
     with pytest.raises(ValidationError, msg='Value must be a boolean (y/n)'):
         assert hooks.to_boolean(None, None, 'spam')
+
+
+def test_validate_packagename():
+    with pytest.raises(AttributeError):
+        hooks.validate_packagename(None)
+
+    configurator = Configurator(
+        template='src/bobtemplates/plone_addon',
+        target_directory='collective.foo')
+    hooks.validate_packagename(configurator)
+
+    with pytest.raises(SystemExit):
+        configurator = Configurator(
+            template='src/bobtemplates/plone_addon',
+            target_directory='foo')
+        hooks.validate_packagename(configurator)
+
+    with pytest.raises(SystemExit):
+        configurator = Configurator(
+            template='src/bobtemplates/plone_addon',
+            target_directory='collective.foo.bar.spam')
+        hooks.validate_packagename(configurator)
+
+    with pytest.raises(SystemExit):
+        configurator = Configurator(
+            template='src/bobtemplates/plone_addon',
+            target_directory='.collective.foo')
+        hooks.validate_packagename(configurator)
+
+    with pytest.raises(SystemExit):
+        configurator = Configurator(
+            template='src/bobtemplates/plone_addon',
+            target_directory='collective.foo.')
+        hooks.validate_packagename(configurator)
+
+    with pytest.raises(SystemExit):
+        configurator = Configurator(
+            template='src/bobtemplates/plone_addon',
+            target_directory='collective.$PAM')
+        hooks.validate_packagename(configurator)
+
+
+def test_pre_username():
+    with pytest.raises(AttributeError):
+        hooks.pre_username(None, None)
+
+    configurator = Configurator(
+        template='src/bobtemplates/plone_addon',
+        target_directory='collective.foo')
+    hooks.pre_username(configurator, None)
+
+
+def test_pre_email():
+    configurator = Configurator(
+        template='src/bobtemplates/plone_addon',
+        target_directory='collective.foo')
+    hooks.pre_email(configurator, None)
+
+
+def test_post_plone_version():
+    configurator = Configurator(
+        template='src/bobtemplates/plone_addon',
+        target_directory='collective.foo')
+    hooks.post_plone_version(configurator, None, '4.3')
+
+    configurator = Configurator(
+        template='src/bobtemplates/plone_addon',
+        target_directory='collective.foo')
+    hooks.post_plone_version(configurator, None, '4-latest')
+
+    configurator = Configurator(
+        template='src/bobtemplates/plone_addon',
+        target_directory='collective.foo')
+    hooks.post_plone_version(configurator, None, '5.1')
+
+    configurator = Configurator(
+        template='src/bobtemplates/plone_addon',
+        target_directory='collective.foo')
+    hooks.post_plone_version(configurator, None, '5-latest')
+
+    configurator = Configurator(
+        template='src/bobtemplates/plone_addon',
+        target_directory='collective.foo',
+        variables={
+            'plone.is_plone5': True,
+            'plone.minor_version': '5.0'
+        })
+    hooks.post_plone_version(configurator, None, '5.0.1')
+
+
+def test_post_ask():
+    configurator = Configurator(
+        template='src/bobtemplates/plone_addon',
+        target_directory='collective.foo',
+        variables={
+            'plone.is_plone5': True,
+            'plone.version': '5.0.1'
+        })
+    hooks.post_ask(configurator)
+
+    configurator = Configurator(
+        template='src/bobtemplates/plone_addon',
+        target_directory='collective.foo')
+    hooks.post_ask(configurator)
+
+
+def test_pre_dexterity_type_name():
+    configurator = Configurator(
+        template='src/bobtemplates/plone_addon',
+        target_directory='collective.foo',
+        variables={
+            'package.type': 'Dexterity'
+        })
+
+    hooks.pre_dexterity_type_name(configurator, None)
+
+    configurator = Configurator(
+        template='src/bobtemplates/plone_addon',
+        target_directory='collective.foo',
+        variables={
+            'package.type': 'ArcheTpye'
+        })
+    with pytest.raises(SkipQuestion):
+        hooks.pre_dexterity_type_name(configurator, None)
 
 
 def test_post_dexterity_type_name():
@@ -112,87 +180,3 @@ def test_post_dexterity_type_name():
     assert hookit(u'Supertype') == u'Supertype'
     assert hookit(u'second_coming') == u'second_coming'
     assert hookit(u'the_2nd_coming') == u'the_2nd_coming'
-
-
-# @pytest.mark.usefixtures("tmpdir")
-class PloneAddTest(unittest.TestCase):
-
-    def setUp(self):
-
-        self.tempdir = tempfile.mkdtemp()
-        self.addCleanup(shutil.rmtree, self.tempdir)
-        generate_plone_addon_template(self.tempdir, 'collective', '', 'foo')
-        result = subprocess.call(
-            ['mrbob', '-O', 'collective.foo', 'bobtemplates:plone_addon',
-             '--config', 'answers.ini'], cwd=self.tempdir
-        )
-        assert result == 0
-
-    def test_validate_packagename(self):
-        with pytest.raises(AttributeError):
-            hooks.validate_packagename(None)
-
-        configurator = Configurator(
-            template='src/bobtemplates/plone_addon',
-            target_directory='collective.foo')
-        hooks.validate_packagename(configurator)
-
-        with pytest.raises(SystemExit):
-            configurator = Configurator(
-                template='src/bobtemplates/plone_addon',
-                target_directory='foo')
-            hooks.validate_packagename(configurator)
-
-        with pytest.raises(SystemExit):
-            configurator = Configurator(
-                template='src/bobtemplates/plone_addon',
-                target_directory='collective.foo.bar.spam')
-            hooks.validate_packagename(configurator)
-
-        with pytest.raises(SystemExit):
-            configurator = Configurator(
-                template='src/bobtemplates/plone_addon',
-                target_directory='.collective.foo')
-            hooks.validate_packagename(configurator)
-
-        with pytest.raises(SystemExit):
-            configurator = Configurator(
-                template='src/bobtemplates/plone_addon',
-                target_directory='collective.foo.')
-            hooks.validate_packagename(configurator)
-
-        with pytest.raises(SystemExit):
-            configurator = Configurator(
-                template='src/bobtemplates/plone_addon',
-                target_directory='collective.$PAM')
-            hooks.validate_packagename(configurator)
-
-    def test_pre_username(self):
-        with pytest.raises(AttributeError):
-            hooks.pre_username(None, None)
-
-        configurator = Configurator(
-            template='src/bobtemplates/plone_addon',
-            target_directory='collective.foo')
-        hooks.pre_username(configurator, None)
-
-    def test_pre_email(self):
-        configurator = Configurator(
-            template='src/bobtemplates/plone_addon',
-            target_directory='collective.foo')
-        hooks.pre_email(configurator, None)
-
-    def test_post_plone_version(self):
-        configurator = Configurator(
-            template='src/bobtemplates/plone_addon',
-            target_directory='collective.foo',
-            variables=None)
-        hooks.post_plone_version(configurator, None, '5.1')
-
-        configurator = Configurator(
-            template='src/bobtemplates/plone_addon',
-            target_directory='collective.foo',
-            variables={
-                'plone_version': '5'
-            })
-        hooks.post_plone_version(configurator, None, '4.3')

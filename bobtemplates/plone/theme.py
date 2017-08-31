23478fb32dc5b51599306fc7139ce87a267ad8ff
MrTango <md@derico.de>
2017-08-31 18:09:09 +0300

    update setup.py and configure.zcml with placeholders

--- bobtemplates/plone/theme.py
+++ bobtemplates/plone/theme.py
@@ -3,6 +3,8 @@
 from mrbob.bobexceptions import ValidationError
 from bobtemplates.plone.base import base_prepare_renderer
 from bobtemplates.plone.base import logger
+from bobtemplates.plone.base import update_file
+from bobtemplates.plone.base import is_string_in_file
 from lxml import etree
 import re
 
@@ -19,38 +21,6 @@ def post_theme_name(configurator, question, answer):
     return answer
 
 
-def _update_configure_zcml(configurator):
-    """ Add the theme to configure.zcml of the package.
-    """
-    namespaces = {'plone': 'http://namespaces.plone.org/plone'}
-    nsprefix = "{{{0}}}".format(namespaces['plone'])
-    file_name = u'configure.zcml'
-    file_path = configurator.variables['package_folder'] +\
-        '/' + file_name
-
-    with open(file_path, 'r') as xml_file:
-        parser = etree.XMLParser(remove_blank_text=True)
-        tree = etree.parse(xml_file, parser)
-        tree_root = tree.getroot()
-        theme_name = configurator.variables['theme.normalized_name']
-        theme_xpath = "./plone:static[@name='{0}']".format(theme_name)
-        if len(tree_root.xpath(theme_xpath, namespaces=namespaces)):
-            print("%s already in configure.zcml, skip adding!" % theme_name)
-            return
-        etree.SubElement(
-            tree_root,
-            nsprefix + 'static',
-            {'name': theme_name, 'directory': 'theme', 'type': 'theme'},
-            nsmap=namespaces
-
-        )
-
-    with open(file_path, 'w') as xml_file:
-        tree.write(
-            xml_file, pretty_print=True, xml_declaration=True,
-            encoding="utf-8")
-
-
 def prepare_renderer(configurator):
     logger.info("Using plone_theme template:")
     configurator = base_prepare_renderer(configurator)
@@ -65,16 +35,50 @@ def prepare_renderer(configurator):
             configurator.variables.get('theme.name')).lower()
 
 
+def _update_configure_zcml(configurator):
+    file_name = u'configure.zcml'
+    file_path = configurator.variables['package_folder'] + '/' + file_name
+    namespaces = {'plone': 'http://namespaces.plone.org/plone'}
+
+    with open(file_path, 'r') as xml_file:
+        parser = etree.XMLParser(remove_blank_text=True)
+        tree = etree.parse(xml_file, parser)
+        tree_root = tree.getroot()
+        theme_name = configurator.variables['theme.normalized_name']
+        theme_xpath = "./plone:static[@name='{0}']".format(theme_name)
+        if len(tree_root.xpath(theme_xpath, namespaces=namespaces)):
+            print("%s already in configure.zcml, skip adding!" % theme_name)
+            return
+
+    match_str = "-*- extra stuff goes here -*-"
+    insert_str = """
+  <plone:static
+      directory="theme"
+      type="theme"
+      name="{0}"
+      />
+
+        """.format(configurator.variables['theme.normalized_name'])
+    update_file(configurator, file_path, match_str, insert_str)
+
+
+def _update_setup_py(configurator):
+    file_name = u'setup.py'
+    file_path = configurator.variables['package.root_folder'] + '/' + file_name
+    match_str = "-*- Extra requirements: -*-"
+    insert_strings = [
+        'collective.themesitesetup',
+        'collective.themefragments',
+    ]
+    for insert_str in insert_strings:
+        insert_str = "        '{0}',\n".format(insert_str)
+        if is_string_in_file(configurator, file_path, insert_str):
+            continue
+        update_file(configurator, file_path, match_str, insert_str)
+
+
 def post_renderer(configurator):
     """
     """
     _update_configure_zcml(configurator)
-    print("""
-    Your theme is ready to go, but you want to make sure that the following
-    packages are added to the ``install_requires`` list in your setup.py
-    and run buildout after adding them:
-
-     - collective.themesitesetup
-     - collective.themefragments
-
-    """)
+    _update_setup_py(configurator)

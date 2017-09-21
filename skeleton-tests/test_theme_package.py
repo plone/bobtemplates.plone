35eefe77158582f6fffbe401f197d8ff34f1d1b1
MrTango <md@derico.de>
2017-09-21 18:36:24 +0300

    refacture pytest/tox setup

---
+++ skeleton-tests/test_theme_package.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+
+from base import dummy_contextmanager
+from base import file_exists
+from base import generate_answers_ini
+
+import glob
+import os.path
+import subprocess
+
+
+base_files = [
+    '.editorconfig',
+    'setup.py',
+    'setup.cfg',
+]
+
+
+addon_files = [
+    'src/__init__.py',
+    'src/plonetheme/blacksea/theme/manifest.cfg'
+]
+
+
+def test_theme_package(tmpdir, capsys, config):
+    template = """[variables]
+package.description = Dummy package
+theme.name = "Black Sea theme"
+author.name = The Plone Collective
+author.email = collective@plone.org
+author.github.user = collective
+
+plone.version = {version}
+""".format(
+        version=config.version,
+    )
+    generate_answers_ini(tmpdir.strpath, template)
+
+    config.template = "theme_package"
+    config.package_name = "plonetheme.blacksea"
+
+    result = subprocess.call(
+        [
+            'mrbob',
+            '-O', config.package_name,
+            'bobtemplates.plone:' + config.template,
+            '--config', 'answers.ini',
+            '--non-interactive',
+        ],
+        cwd=tmpdir.strpath,
+    )
+    assert result == 0
+
+    generated_files = glob.glob(
+        tmpdir.strpath + '/' + config.package_name + '/*',
+    )
+    length = len(tmpdir.strpath + '/' + config.package_name + '/')
+    generated_files = [f[length:] for f in generated_files]
+    required_files = base_files + addon_files
+    assert required_files <= generated_files
+
+    base_path = tmpdir.strpath + '/' + config.package_name
+
+    assert file_exists(
+        base_path, '/src/plonetheme/blacksea/theme/manifest.cfg')
+
+    wd = os.path.abspath(
+        os.path.join(tmpdir.strpath, config.package_name))
+
+    with capsys.disabled() if config.verbose else dummy_contextmanager():
+        setup_virtualenv_result = subprocess.call(
+            [
+                'virtualenv',
+                '.'
+            ],
+            cwd=wd,
+        )
+        assert setup_virtualenv_result == 0
+        install_buildout_result = subprocess.call(
+            [
+                './bin/pip',
+                'install',
+                '-U',
+                '-r',
+                'requirements.txt',
+            ],
+            cwd=wd,
+        )
+        assert install_buildout_result == 0
+        annotate_result = subprocess.call(
+            ['bin/buildout', 'annotate', ],
+            cwd=wd,
+        )
+        assert annotate_result == 0
+        buildout_result = subprocess.call(
+            ['bin/buildout', ],
+            cwd=wd,
+        )
+        assert buildout_result == 0
+        test_result = subprocess.call(
+            ['bin/test', ],
+            cwd=wd,
+        )
+        assert test_result == 0
+        test__code_convention_result = subprocess.call(
+            ['bin/code-analysis', ],
+            cwd=wd,
+        )
+        assert test__code_convention_result == 0

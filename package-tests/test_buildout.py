0d040d532730a16a4afdaf503f2f28dfa76946a2
Alexander Loechel <Alexander.Loechel@lmu.de>
2017-09-26 00:59:57 +0200

    more tests --> better coverage

---
+++ package-tests/test_buildout.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from bobtemplates.plone import buildout
+from mrbob.configurator import Configurator
+
+
+def test_prepare_renderer():
+    configurator = Configurator(
+        template='bobtemplates.plone:buildout',
+        target_directory='collective.foo',
+    )
+    buildout.prepare_renderer(configurator)
+    assert configurator.variables['template_id'] == 'buildout'
+
+
+def test_post_renderer():
+    buildout.post_renderer(None)

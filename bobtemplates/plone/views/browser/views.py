345484e128748c6f48783e69eb49893faed2d7b2
Saicharan Reddy <saicharan.reddy1@gmail.com>
2018-03-03 16:31:00 +0000

    Added a view template for talks and events

--- bobtemplates/plone/views/browser/views.py
+++ bobtemplates/plone/views/browser/views.py
@@ -1,12 +1,39 @@
-""" Example view
-"""
-
-# Zope imports
-from zope.interface import Interface
+# -*- coding: utf-8 -*-
 from Products.Five.browser import BrowserView
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from operator import itemgetter
 
-class MyView(BrowserView):
-    """ Render the title and description of item only (example)
-    """
-    index = ViewPageTemplateFile("myview.pt")
+
+class DemoView(BrowserView):
+    """A demo listing"""
+
+    def the_title(self):
+        return u'A list of talks:'
+
+    def talks(self):
+        results = []
+        data = [
+            {'title': 'Dexterity is the new default!',
+             'subjects': ('content-types', 'dexterity')},
+            {'title': 'Mosaic will be the next big thing.',
+             'subjects': ('layout', 'deco', 'views'),
+             'url': 'https://www.youtube.com/watch?v=QSNufxaYb1M'},
+            {'title': 'The State of Plone',
+             'subjects': ('keynote',)},
+            {'title': 'Diazo is a powerful tool for theming!',
+             'subjects': ('design', 'diazo', 'xslt')},
+            {'title': 'Magic templates in Plone 5',
+             'subjects': ('templates', 'TAL'),
+             'url': 'http://www.starzel.de/blog/magic-templates-in-plone-5'},
+        ]
+        for item in data:
+            try:
+                url = item['url']
+            except KeyError:
+                url = 'https://www.google.com/search?q=%s' % item['title']
+            talk = dict(
+                title=item['title'],
+                subjects=', '.join(item['subjects']),
+                url=url
+            )
+            results.append(talk)
+        return sorted(results, key=itemgetter('title'))

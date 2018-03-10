35894aa0572296ddd4dd47e2e6dce8de2179a1b6
Saicharan Reddy <saicharan.reddy1@gmail.com>
2018-03-10 19:34:05 +0000

    Simpler view for debugging

--- bobtemplates/plone/collective.view/src/collective/view/browser/views/views.py
+++ bobtemplates/plone/collective.view/src/collective/view/browser/views/views.py
@@ -8,18 +8,3 @@ class DemoView(BrowserView):
     def the_title(self):
         return u'A list of talks:'
 
-    def talks(self):
-        results = []
-        data = [{'title': 'Dexter'}, {'title': 'Plone'}]
-        for item in data:
-            try:
-                url = item['url']
-            except KeyError:
-                url = 'google.com'
-            talk = dict(
-                title=item['title'],
-                subjects=', '.join(item['subjects']),
-                url=url
-            )
-            results.append(talk)
-        return sorted(results, key=itemgetter('title'))

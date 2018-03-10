c3f57ae5fd1376b97b22c632cb3435653633d2f5
Saicharan Reddy <saicharan.reddy1@gmail.com>
2018-03-10 18:29:42 +0000

    Minor fixes

--- bobtemplates/plone/collective.view/src/collective/view/browser/views/views.py
+++ bobtemplates/plone/collective.view/src/collective/view/browser/views/views.py
@@ -8,17 +8,9 @@ class DemoView(BrowserView):
     def the_title(self):
         return u'A list of talks:'
 
-    def talks(self,):
+    def talks(self):
         results = []
-        data = [{'title': 'Dexterity is the new default!',
-                 'subjects': ('content-types', 'dexterity')},
-                {'title': 'Mosaic will be the next big thing.',
-                 'subjects': ('layout', 'deco', 'views'),
-                 'url': 'https://www.youtube.com/watch?v=QSNufxaYb1M'},
-                {'title': 'The State of Plone', 'subjects': ('keynote',)},
-                {'title': 'Diazo is a powerful tool for theming!',
-                 'subjects': ('design', 'diazo', 'xslt')},
-                ]
+        data = [{'title':'Dexter'},{'title':'Plone'}]
         for item in data:
             try:
                 url = item['url']

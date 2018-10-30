1a76769b52320b8a0e9ffc0399297f964c53b999
MrTango <md@derico.de>
2018-10-30 21:08:52 +0200

    Fix slugify method and add some utils methods like safe_unicode

--- bobtemplates/plone/portlet.py
+++ bobtemplates/plone/portlet.py
@@ -4,8 +4,8 @@
 from bobtemplates.plone.base import base_prepare_renderer
 from bobtemplates.plone.base import git_commit
 from bobtemplates.plone.base import update_file
+from bobtemplates.plone.utils import slugify
 from lxml import etree
-from base import slugify
 
 import case_conversion as cc
 import os

877cd52ce7c813893f1bd651395e3777a83009b9
MrTango <md@derico.de>
2019-05-30 21:15:37 +0300

    fix linting

--- package_tests/test_subscriber.py
+++ package_tests/test_subscriber.py
@@ -2,7 +2,7 @@
 
 """Test view generation."""
 
-from bobtemplates.plone import base
+# from bobtemplates.plone import base
 from bobtemplates.plone import subscriber
 from mrbob.bobexceptions import ValidationError
 from mrbob.configurator import Configurator
@@ -13,9 +13,9 @@ import pytest
 
 def test_update_subscribers_configure_zcml(tmpdir):
     """Test configure changes when changes are already in place."""
-    target_path = tmpdir.strpath + '/collective.sample'
-    package_path = target_path + '/src/collective/sample'
-    subscribers_path = package_path + '/subscribers/'
+    target_path = tmpdir.strpath + "/collective.sample"
+    package_path = target_path + "/src/collective/sample"
+    subscribers_path = package_path + "/subscribers/"
     os.makedirs(target_path)
     os.makedirs(package_path)
     os.makedirs(subscribers_path)
@@ -32,24 +32,21 @@ def test_update_subscribers_configure_zcml(tmpdir):
 
 </configure>
 """
-    with open(os.path.join(subscribers_path + 'configure.zcml'), 'w') as f:
+    with open(os.path.join(subscribers_path + "configure.zcml"), "w") as f:
         f.write(template)
     configurator = Configurator(
-        template='bobtemplates.plone:subscriber',
-        target_directory='collective.sample',
-        bobconfig={
-            'non_interactive': True,
-        },
+        template="bobtemplates.plone:subscriber",
+        target_directory="collective.sample",
+        bobconfig={"non_interactive": True},
         variables={
-            'subscriber_handler_name': 'obj_mod_do_something',
-            'subscriber_handler_file_name': 'obj_mod_do_something',
-            'package_folder': package_path,
+            "subscriber_handler_name": "obj_mod_do_something",
+            "subscriber_handler_file_name": "obj_mod_do_something",
+            "package_folder": package_path,
         },
     )
     subscriber._update_subscribers_configure_zcml(configurator)
 
-    with open(os.path.join(subscribers_path + 'configure.zcml'), 'r') as f:
+    with open(os.path.join(subscribers_path + "configure.zcml"), "r") as f:
         content = f.read()
         if content != template:
             pytest.raises(ValidationError)
-

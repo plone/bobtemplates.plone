06e4818291e9d433bca450483551d6c3b240f037
Peter Holzer <peter.holzer@agitator.com>
2019-06-20 15:50:39 +0200

    fix sorting

--- bobtemplates/plone/theme_barceloneta.py
+++ bobtemplates/plone/theme_barceloneta.py
@@ -3,9 +3,9 @@
 from bobtemplates.plone.base import base_prepare_renderer
 from bobtemplates.plone.base import echo
 from bobtemplates.plone.base import git_commit
+from bobtemplates.plone.base import is_string_in_file
 from bobtemplates.plone.base import update_file
 from bobtemplates.plone.base import validate_packagename
-from bobtemplates.plone.base import is_string_in_file
 from lxml import etree
 from mrbob.bobexceptions import ValidationError
 

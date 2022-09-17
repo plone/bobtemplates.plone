c427411d73068336cff604cdfa7535f6f4e086af
Johannes Raggam <thetetet@gmail.com>
2022-09-17 23:54:27 +0200

    fwd

--- bobtemplates/plone/mockup_pattern/resources/index.js
+++ bobtemplates/plone/mockup_pattern/resources/index.js
@@ -1,2 +1,5 @@
 // Webpack entry point for module federation.
-import "./bundle.js";
+import "@patternslib/patternslib/webpack/module_federation";
+// The next import needs to be kept with brackets, otherwise we get this error:
+// "Shared module is not available for eager consumption."
+import("./bundle-config");

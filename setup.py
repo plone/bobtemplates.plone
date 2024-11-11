9fd4df8a25bb18b2ac71615a2e4c16f6d6f78461
MrTango <md@derico.de>
2024-11-11 17:29:43 +0200

    set python_requires=">=3.8

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
     namespace_packages=["bobtemplates"],
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=3.9",
+    python_requires=">=3.8",
     install_requires=[
         "setuptools",
         "mr.bob",
